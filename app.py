7fa40e4876a04d5e955383baa983d01f1678300f
aman <amansomething@gmail.com>
2025-02-10 12:24:40 -0500

    Working initial state

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/test')
+def test():
+    return 'Test!'
+
+
 if __name__ == '__main__':
-    app.run()
+    host = os.environ.get("HOST", "0.0.0.0")
+    port = os.environ.get("PORT", "8000")
+
+    app.run(debug=True, host=host, port=port)
